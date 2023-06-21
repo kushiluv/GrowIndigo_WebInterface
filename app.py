fc8aa5a094f43bfd9ea52df44ed38d5e7916930a
Kushiluv Jangu <88649199+kushiluv@users.noreply.github.com>
2023-06-21 14:53:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask,jsonify, render_template, url_for, request
+import uuid
+import requests
+from flask import Flask, render_template, session, request, redirect, url_for
+from flask_session import Session 
+import msal
+import app_config
 import json
+import time
 import pandas as pd
 import math
 import numpy as np
@@ -8,6 +14,94 @@ import geopandas as gpd
 from shapely.geometry.polygon import orient
 from shapely.geometry import Polygon
 app = Flask(__name__)
+app.config.from_object(app_config)
+Session(app)
+
+# This section is needed for url_for("foo", _external=True) to automatically
+# generate http scheme when this sample is running on localhost,
+# and to generate https scheme when it is deployed behind reversed proxy.
+# See also https://flask.palletsprojects.com/en/1.0.x/deploying/wsgi-standalone/#proxy-setups
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+@app.route("/")
+def index():
+    if not session.get("user"):
+        return redirect(url_for("login"))
+    return render_template('index.html', user=session["user"], version=msal.__version__)
+
+@app.route("/login")
+def login():
+    # Technically we could use empty list [] as scopes to do just sign in,
+    # here we choose to also collect end user consent upfront
+    session["flow"] = _build_auth_code_flow(scopes=app_config.SCOPE)
+    return render_template("login.html", auth_url=session["flow"]["auth_uri"], version=msal.__version__)
+
+@app.route(app_config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
+def authorized():
+    try:
+        cache = _load_cache()
+        result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
+            session.get("flow", {}), request.args)
+        if "error" in result:
+            return render_template("auth_error.html", result=result)
+        session["user"] = result.get("id_token_claims")
+        _save_cache(cache)
+        
+    except ValueError:  # Usually caused by CSRF
+        pass  # Simply ignore them
+    return redirect(url_for('index1'))
+
+@app.route("/logout")
+def logout():
+    session.clear()  # Wipe out user and its token cache from session
+    return redirect(  # Also logout from your tenant's web session
+        app_config.AUTHORITY + "/oauth2/v2.0/logout" +
+        "?post_logout_redirect_uri=" + url_for("index", _external=True))
+
+@app.route("/graphcall")
+def graphcall():
+    token = _get_token_from_cache(app_config.SCOPE)
+    if not token:
+        return redirect(url_for("login"))
+    graph_data = requests.get(  # Use token to call downstream service
+        app_config.ENDPOINT,
+        headers={'Authorization': 'Bearer ' + token['access_token']},
+        ).json()
+    return render_template('display.html', result=graph_data)
+
+
+def _load_cache():
+    cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
+    return cache
+
+def _save_cache(cache):
+    if cache.has_state_changed:
+        session["token_cache"] = cache.serialize()
+
+def _build_msal_app(cache=None, authority=None):
+    return msal.ConfidentialClientApplication(
+        app_config.CLIENT_ID, authority=authority or app_config.AUTHORITY,
+        client_credential=app_config.CLIENT_SECRET, token_cache=cache)
+
+def _build_auth_code_flow(authority=None, scopes=None):
+    return _build_msal_app(authority=authority).initiate_auth_code_flow(
+        scopes or [],
+        redirect_uri=url_for("authorized", _external=True))
+
+def _get_token_from_cache(scope=None):
+    cache = _load_cache()  # This web app maintains one cache per session
+    cca = _build_msal_app(cache=cache)
+    accounts = cca.get_accounts()
+    if accounts:  # So all account(s) belong to the current signed-in user
+        result = cca.acquire_token_silent(scope, account=accounts[0])
+        _save_cache(cache)
+        return result
+
+app.jinja_env.globals.update(_build_auth_code_flow=_build_auth_code_flow)  # Used in template
+
 
 def gpd_geographic_area(geodf):
     if not geodf.crs or not geodf.crs.is_geographic:
@@ -84,11 +178,18 @@ def read_excel_data():
     
     return data
 
-@app.route('/')
-def index():
+
+@app.route('/index1')
+def index1():
     data = read_excel_data()
     data_json = json.dumps(data)  # Convert data to JSON string
-    return render_template('index.html', data=data_json)
+    return render_template('index1.html', data=data_json)
+
+@app.route('/redirect_index1', methods=['GET', 'POST'])
+def redirect_index1():
+    print("hjere")
+    time.sleep(1)  # Delay the redirect for 1 second
+    return redirect(url_for('index1'))
 
 @app.route('/filter', methods=['POST'])
 def filter_polygons():
@@ -123,7 +224,7 @@ def filter_polygons():
     
     data_json = json.dumps(data)  # Convert filtered data to JSON string
     
-    return render_template('index.html', data=data_json, center_lat=center_lat, center_lng=center_lng, zoom=zoom)
+    return render_template('index1.html', data=data_json, center_lat=center_lat, center_lng=center_lng, zoom=zoom)
 
 @app.route('/save_validation', methods=['POST'])
 def save_polygon_validation():
@@ -251,7 +352,6 @@ def calculate_center_coordinates(data, state=None, district=None,mdo_id=None):
         # Default center coordinates and zoom level if no data available
         return 20.5937, 78.9629, 5  # Center of India with a default zoom level of 5
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
